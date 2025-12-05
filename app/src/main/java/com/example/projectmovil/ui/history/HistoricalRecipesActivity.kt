fd1b65ca60b6440cf8e425a38dffd8d512e3bee2
MANNLAR <m1aguilar@outlook.com>
2025-12-04 23:51:09 -0600

    Carpetas
    abc

--- app/src/main/java/com/example/projectmovil/ui/history/HistoricalRecipesActivity.kt
+++ app/src/main/java/com/example/projectmovil/ui/history/HistoricalRecipesActivity.kt
@@ -1,10 +1,17 @@
-package com.example.projectmovil
+package com.example.projectmovil.ui.history
 
 import android.app.AlertDialog
 import android.os.Bundle
 import android.view.View
-import android.widget.*
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RatingBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.projectmovil.R
 import com.example.projectmovil.data.HistoricalRecipe
 import com.example.projectmovil.data.HistoricalRecipeProvider
 
